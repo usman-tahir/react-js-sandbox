4a92fd771f6343052d03d18f093264c2d45a4810
usman-tahir <tahir.usman.ali94@outlook.com>
2017-04-29 15:48:56 -0400

    configured webpack config

---
+++ react-base/webpack.config.js
@@ -0,0 +1,35 @@
+
+(function () {
+
+  'use strict';
+
+  var config = {
+    entry: './main.js',
+
+    output: {
+      path: './',
+      filename: 'index.js'
+    },
+
+    devServer: {
+      inline: true,
+      port: 8080
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: {
+            presets:['es2015', 'react']
+          }
+        }
+      ]
+    }
+  }
+
+  module.exports = config;
+
+})();
