c0433f4a4ec1e822a97de3f365da01e9bfd4a318
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-02 16:20:14 -0400

    created the Board and Square Components; removed the template index.jsx file

---
+++ tic-tac-toe/src/client/app/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'React';
+
+class Square extends React.Component {
+  render() {
+    return(
+      <button className="square">
+        /* TODO */
+      </button>
+    );
+  }
+}
