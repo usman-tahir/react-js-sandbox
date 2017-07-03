de231e06edb0ba5f474c6f9c2f2de9839daac56a
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-07-02 20:21:51 -0400

    updated the color code generation for the moves

--- tic-tac-toe/src/client/app/Square.jsx
+++ tic-tac-toe/src/client/app/Square.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function Square(props) {
+	// spaces are required since the class is being appended manually
+	const classes = {'X': ' x', 'O': ' o'};
+
   return(
-    <button className={'square' + (props.value === 'X' ? ' x' : ' o')} onClick={props.onClick} >
+    <button className={'square' + (props.value ? classes[props.value] : '')} onClick={props.onClick} >
       {props.value}
     </button>
   );
