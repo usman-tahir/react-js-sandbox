c1026a414af592cc92858d1ecea6b7ea8474e854
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-04 09:51:27 -0400

    initial commit and setup for the React todo application

---
+++ todo-app/src/client/app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>hello world</p>
+			</div>
+		);
+	}
+}
+
+render(<App/>, document.getElementById('container'));
